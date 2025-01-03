cd1a0c9fdf9cbc5c0739614fff3dcdc0d2a3cd8d
ManU4kym <emmannjoroge36@gmail.com>
2025-01-03 15:05:43 +0300

    component with logout button

---
+++ components/AppHeader.vue
@@ -0,0 +1,27 @@
+<template>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li v-if="!isAuthenticated"><a href="/login">Login</a></li>
+                <li v-if="isAuthenticated"><button @click="logout">Logout</button></li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script setup>
+import { useAuth } from '@/composables/useAuth';
+const { isAuthenticated, logout } = useAuth();
+</script>
+
+<style scoped>
+nav ul {
+    display: flex;
+    list-style: none;
+}
+
+nav li {
+    margin-right: 20px;
+}
+</style>
